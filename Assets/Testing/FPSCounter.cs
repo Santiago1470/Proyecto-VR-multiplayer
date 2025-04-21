57f7671e1ed0ab3d1abd591ecbc0e6dec53e312d
Santiago1470 <st147086@gmail.com>
2025-04-20 22:00:57 -0500

    Se agregró texto y script para ver los FPS en ejecución, además se optimizó un poco escenario NASA

---
+++ Assets/Testing/FPSCounter.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public TextMeshProUGUI fpsText;
+    private float deltaTime = 0.0f;
+    private float timer = 0.0f;
+    private float refreshRate = 0.15f; // Tiempo entre actualizaciones, en segundos
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        timer += Time.unscaledDeltaTime;
+
+        if (timer >= refreshRate)
+        {
+            float fps = 1.0f / deltaTime;
+            fpsText.text = $"FPS: {Mathf.CeilToInt(fps)}";
+            timer = 0f;
+        }
+    }
+}
+
